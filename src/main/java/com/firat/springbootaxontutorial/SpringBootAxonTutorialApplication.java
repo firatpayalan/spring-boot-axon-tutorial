5b3c89cf70e8888b85e2959e4a4a5f0a9e621381
Fırat Payalan <yfpayalan@gmail.com>
2020-07-29 23:17:33 +0300

    add postgres and rabbit implementation

--- src/main/java/com/firat/springbootaxontutorial/SpringBootAxonTutorialApplication.java
+++ src/main/java/com/firat/springbootaxontutorial/SpringBootAxonTutorialApplication.java
@@ -3,6 +3,7 @@ package com.firat.springbootaxontutorial;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.axonframework.springboot.autoconfig.AxonServerAutoConfiguration;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,6 +25,26 @@ public class SpringBootAxonTutorialApplication {
         @Bean
         public EventStorageEngine eventStorageEngine(){
             return new InMemoryEventStorageEngine();
+        }
+    }
+
+    @Configuration
+    static class RabbitInitializer{
+        public static final String QUEUE_NAME = "wallet.events.queue";
+        public static final String EXCHANGE_NAME = "wallet.events";
+        @Bean
+        public Queue queue() {
+            return new Queue(QUEUE_NAME, false);
+        }
+
+        @Bean
+        public FanoutExchange exchange() {
+            return new FanoutExchange(EXCHANGE_NAME);
+        }
+
+        @Bean
+        Binding binding(Queue queue, FanoutExchange exchange) {
+            return BindingBuilder.bind(queue).to(exchange);
         }
 
     }
